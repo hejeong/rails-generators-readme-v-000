83eeaa336ebe37cf70e1797973ae5b00bb19db24
Jonathan Hong <jonhong777@gmail.com>
2018-09-08 04:34:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class ReleaseYearValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
 
+  end
 end
 class Song < ActiveRecord::Base
   validates :title, presence: true
